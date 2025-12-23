3f7f91ef29cc50ec75eec1d71ccd9da545ee492d
AvantikaChaudhary-1416 <avantikachaudhary688@gmail.com>
2025-12-23 18:08:13 +0530

    new

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(" Maven Build Successful ");
+        System.out.println(" Maven Build Successful hello");
     }
 }
